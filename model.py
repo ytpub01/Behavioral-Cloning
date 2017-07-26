758c740126d569988e840de0f104189cb8b62ca2
Yasser Taima <ytpub@yahoo.com>
2017-07-25 21:49:56 -0700

    converted RGB image to BGR as images during autonomous driving are read in RGB but during training in BGR

--- model.py
+++ model.py
@@ -1,13 +1,21 @@
+# Udacity Self-Driving car term 1 project on behavioral neural networks
+# contact ytpub@yahoo.com
+# last edit 7/25/2017
+
 import csv
 import cv2
 import numpy as np
 
+# read in steering data
 lines = []
 with open('../data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
     lines.append(line)
 
+# read out measurements from driving log
+# if image is from left or right vantage point, use correction
+# to adjust
 images = []
 measurements = []
 correction = 0.2
@@ -25,6 +33,7 @@ for line in lines:
       measurement -= correction
     measurements.append(measurement)
 
+# add flipped images to data for more balanced training
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
   augmented_images.append(image)
@@ -32,6 +41,7 @@ for image, measurement in zip(images, measurements):
   augmented_images.append(cv2.flip(image, 1))
   augmented_measurements.append(-measurement)
 
+# set up training data
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
@@ -39,23 +49,34 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Convolution2D, Cropping2D
 
-# This convolutional network stack is based on the nvidia model
+# The following convolutional neural network stack
+# is based on the NVIDIA iarchitecture for autonomous vehicles
 model = Sequential()
+# normalization layer
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
+# convolution layers
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
+# connected layers
 model.add(Dense(1164))
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# adam optimizer instead of gradient descent finds learning rate
+# automatically 
 model.compile(loss='mse', optimizer='adam')
+# run the network on the training data with random shuffling of
+# so that training and validation sets are different each time
+# it is run.
+# Number of epochs and validation set size are set to give a rapidly
+# decreasing loss but also to avoid over-fitting
 model.fit(X_train, y_train, validation_split=0.3, shuffle=True, epochs=2)
 
 model.save('model.h5')
